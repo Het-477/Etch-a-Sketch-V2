e0a067b4e9e0ef594b455070f48ec678840681e4
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-05-25 13:27:52 +0530

    feat: add header controls

---
+++ src/HeaderControls.jsx
@@ -0,0 +1,11 @@
+function HeaderControls() {
+    return <div className="toggle-controls">
+        <span className="material-symbols-outlined" title="toggle grid">grid</span>
+        <span className="material-symbols-outlined" title="erase sketchpad">ink_eraser</span>
+        <span className="material-symbols-outlined" title="toggle sketch pen">stylus_note</span>
+        <span className="material-symbols-outlined" title="color palette">palette</span>
+    </div>
+}
+
+
+export default HeaderControls
